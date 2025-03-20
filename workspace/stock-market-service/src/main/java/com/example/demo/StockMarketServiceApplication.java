98d8710a8a05e5a72ff25424df31436e71602ae8
Sanat Patil <sanat.patil@wissen.com>
2025-03-20 16:51:21 +0530

    Final Commit

---
+++ workspace/stock-market-service/src/main/java/com/example/demo/StockMarketServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+@SpringBootApplication
+@EnableScheduling
+public class StockMarketServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockMarketServiceApplication.class, args);
+	}
+
+}
